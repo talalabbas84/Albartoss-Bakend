2fcac11f4ebc62ab69de95eb5cf63e246256b477
talalabbas84 <talalabbas84@gmail.com>
2020-08-17 10:58:15 +0500

    Update the sendTokenREsponse function

--- controllers/auth.js
+++ controllers/auth.js
@@ -287,5 +287,5 @@ const sendTokenResponse = (user, statusCode, res) => {
   res
     .status(statusCode)
     .cookie('token', token, options)
-    .json({ success: true, token });
+    .json({ success: true, token, user });
 };
