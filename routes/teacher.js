43b2e4e032c752f9a3d247b33d669a9cafbf9cab
talalabbas84 <talalabbas84@gmail.com>
2020-08-17 10:33:26 +0500

    Fix the bug

--- routes/teacher.js
+++ routes/teacher.js
@@ -29,12 +29,4 @@ router
 //   .put(protect, authorize('publisher', 'admin'), updateBootcamp)
 //   .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
-// router.post('/login', login);
-// router.get('/logout', logout);
-// router.get('/me', protect, getMe);
-// router.put('/updatedetails', protect, updateDetails);
-// router.put('/updatepassword', protect, updatePassword);
-// router.post('/forgotpassword', forgetPassword);
-// router.get('/resetpassword/:resettoken', resetPassword);
-// router.get('/verifyemail/:verifytoken/:email', verifyEmail);
 module.exports = router;
