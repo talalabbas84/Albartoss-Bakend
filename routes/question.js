ccc35da4525ace76fc402ba22b464bba84d64b88
talalabbas84 <talalabbas84@gmail.com>
2020-08-18 17:55:12 +0500

    changed the authorization of getquestion route

--- routes/question.js
+++ routes/question.js
@@ -4,7 +4,7 @@ const { addQuestion, getQuestions } = require('../controllers/question');
 
 const { protect, authorize } = require('../middleware/auth');
 
-router.route('/').get(protect, authorize('teacher'), getQuestions);
+router.route('/').get(protect, getQuestions);
 router.route('/addquestion').post(protect, authorize('student'), addQuestion);
 
 module.exports = router;
