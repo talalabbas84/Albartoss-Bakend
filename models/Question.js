d869ca956f4fcaecca7b1159206bcc37f324e749
talalabbas84 <talalabbas84@gmail.com>
2020-08-18 17:12:33 +0500

    Changed question schema

--- models/Question.js
+++ models/Question.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const questionSchema = new mongoose.Schema(
   {
-    question: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
       type: String,
       required: true
     },
