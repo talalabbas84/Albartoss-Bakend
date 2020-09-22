cabc5cf00bbcc469681742f5fda814df215e1ca5
talalabbas84@gmail.com <talalabbas84@gmail.com>
2020-09-22 18:48:54 +0500

    Dasdsa

--- controllers/lesson.js
+++ controllers/lesson.js
@@ -118,11 +118,11 @@ exports.lessonStatus = asynchandler(async (req, res, next) => {
 
         description: `${lesson.description}`,
         start: {
-          dateTime: `${lesson.lessonStartTime}`,
+          dateTime: '2020-09-11T09:00:00-07:00',
           timeZone: 'America/Los_Angeles'
         },
         end: {
-          dateTime: `${lesson.lessonEndTime}`,
+          dateTime: '2020-09-11T17:00:00-07-07:00',
           timeZone: 'America/Los_Angeles'
         },
         recurrence: ['RRULE:FREQ=DAILY;COUNT=2'],
